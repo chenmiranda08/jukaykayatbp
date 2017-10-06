dbdabb28896daa2f03a9b596b936e6372cd39f15
chenmiranda08 <30958670+chenmiranda08@users.noreply.github.com>
2017-10-06 17:49:37 +0800

    Update wsgi.py

--- Juks/Juks/wsgi.py
+++ Juks/Juks/wsgi.py
@@ -8,7 +8,12 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
+path = '/home/jukaykayatbp/Juks'
+if path not in sys.path:
+    sys.path.append(path)
+    
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Juks.settings")
